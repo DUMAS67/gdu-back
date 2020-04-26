015cf9fb70a4098eb7474539336ec7f3a4404484
DUMAS <you@example.com>
2020-04-26 13:48:30 +0200

    Gdu-Back recherche criticite

--- src/main/java/dev/pch/repository/CriticiteRepo.java
+++ src/main/java/dev/pch/repository/CriticiteRepo.java
@@ -1,9 +1,15 @@
 package dev.pch.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.pch.domains.Criticite;
 
 public interface CriticiteRepo extends JpaRepository<Criticite, Integer> {
+
+	Optional<Criticite> findByValeur(int valeur);
+
+	Optional<Criticite> findById(int id);
 
 }
